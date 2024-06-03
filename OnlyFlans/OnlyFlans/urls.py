6c03cd911efac4c68566ff4d96d8926477f44881
anibal <anibalvelozo2010@gmail.com>
2024-06-02 23:28:39 -0400

    login 2

--- OnlyFlans/OnlyFlans/urls.py
+++ OnlyFlans/OnlyFlans/urls.py
@@ -1,30 +1,20 @@
-"""
-URL configuration for OnlyFlans project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from web.views import index, bienvenido, acerca, contacto, exito
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('', index, name='index'),
     path('bienvenido/', bienvenido, name='bienvenido'),
     path('acerca/', acerca, name='acerca'),
     path('contacto/', contacto, name='contacto'),
     path('exito/', exito, name='exito'),
-]
 
+    # Agregando rutas para autenticación
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+
+    # Incluyendo las URLs por defecto de auth para que gestionen el resto
+    path('accounts/', include('django.contrib.auth.urls')),
+]
